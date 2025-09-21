00ce9d8b73c5efce0f1d08b5d2265cb05af3bacd
Musashi Moriya <musashi.moriya@gmail.com>
2025-09-21 10:41:20 +0900

    受付時間を表示

--- resources/views/tokushoho/show.blade.php
+++ resources/views/tokushoho/show.blade.php
@@ -23,6 +23,9 @@
             <li>
                 <strong>電話番号:</strong>
                 {{ $tokushohoData['tel'] }}
+                @if(Auth::user()->school->tel_available_time)
+                    <br>受付時間：{{ Auth::user()->school->tel_available_time }}
+                @endif
             </li>
             @endif
 
